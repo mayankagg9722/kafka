3f6b5a8f2aaf39619361eb2ab48eaa1f27d86b9d
Mayank Aggarwal <mayankagg9722@gmail.com>
2025-08-29 21:42:02 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd docker
-pip3 install -r reqiurements.txt
+pip3 install -r requirements.txt
 python3 docker_build_test.py kafka/test --image-tag=3.6.0 --image-type=jvm --kafka-url=https://archive.apache.org/dist/kafka/3.6.0/kafka_2.13-3.6.0.tgz
